622f13b70afcae483885da14091964735ab9f45d
Abhay <abhay03december2003@gmail.com>
2025-11-25 18:51:20 +0530

    Updated MongoDB connection logic in testConnection.js

---
+++ server/testConnection.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+const uri = 'mongodb+srv://abhay:abhay1234@cluster0.nmv8f0n.mongodb.net/mindsphere?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(uri)
+  .then(() => console.log('✅ MongoDB connected successfully'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
